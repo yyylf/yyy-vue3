27557341961e6e65fed5aafcc5bf6865d432fa17
yuyouyao <yuyouyao@jlkj.net>
2023-11-24 19:35:33 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="onClick">click me</button>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script lang="ts" setup>
+// eslint-disable-next-line quotes, semi
+import HelloWorld from "../components/HelloWorld.vue";
+// eslint-disable-next-line quotes, semi
+import axios from "axios";
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class HomeView extends Vue {}
+const onClick = () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, semi
+  console.log(111);
+  // eslint-disable-next-line quotes, semi
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, semi, quotes
+  axios
+    // eslint-disable-next-line @typescript-eslint/no-empty-function, semi, quotes
+    .get("/user1/users")
+    .then(function (response) {
+      // eslint-disable-next-line semi
+      console.log(response.data);
+      // eslint-disable-next-line semi
+    });
+  // eslint-disable-next-line semi
+};
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    proxy: {
+      '/user1': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        // pathRewrite: {
+        //   '^/api': ''
+        // }
+      }
+    }
+  }
 })
